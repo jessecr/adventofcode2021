ef9e0435c1c421004d4fc4300b90203cf67ac7f8
Jesse Capper-Ream <jessecapper@gmail.com>
2021-12-03 22:39:48 -0800

    Day04 improved

--- day04/main.py
+++ day04/main.py
@@ -1,110 +1,37 @@
 import math
 
-input_file = "day04/example_input"
-# input_file = "input"
-text = open(input_file, "r").read()
+input_file = "example_input"
+input_file = "input"
 
-chunks = text.split("\n\n")
+chunks = open(input_file).read().split("\n\n")
 nums = [int(n) for n in chunks[0].split(",")]
-boards = []
-for chunk in chunks[1:]:
-    boards.append([int(n) for n in " ".join(chunk.split()).split()])
+boards = [[int(n) for n in chunk.replace('\n', ' ').split()] for chunk in chunks[1:]]
 
+board_width = int(math.sqrt(len(boards[0])))
 
-def check_board(board):
-    size = int(math.sqrt(len(board)))
-    for r in range(size):
-        if all(board[r : r + size]):
-            print(board[r : r + size])
-            return True
-    for c in range(size):
-        if all([board[i] for i in range(c, len(board), size)]):
-            print([board[i] for i in range(c, len(board), size)])
+def is_board_winning(board):
+    for i in range(board_width):
+        row_start = i * board_width
+        if all(c is None for c in board[row_start:row_start + board_width]) or \
+            all(c is None for c in board[i:len(board):board_width]):
             return True
 
     return False
 
 
-hits = [[0] * len(board) for board in boards]
-winner = None
-for num in nums:
-    for i, board in enumerate(boards):
-        if num in board:
-            idx = board.index(num)
-            hits[i][idx] = 1
-
-    for i, board in enumerate(hits):
-        if check_board(board):
-            print("hit", num)
-            winner = i
+def solve(win_cond):
+    winning_boards = []
+    for num in nums:
+        for i, board in enumerate(boards):
+            if i not in winning_boards and num in board:
+                board[board.index(num)] = None
+                if is_board_winning(board):
+                    winning_boards.append(i)
+        if win_cond(winning_boards):
             break
 
-    if winner:
-        break
-else:
-    print("no winner")
-assert winner
-hit = hits[winner]
-board = boards[winner]
-s = sum([board[i] for i, v in enumerate(hit) if not v])
-print(s * num)
+    board = boards[winning_boards[-1]]
+    return num * sum([i for i in board if i is not None])
 
-
-import math
-
-input_file = "day04/example_input"
-input_file = "day04/input"
-text = open(input_file, "r").read()
-
-chunks = text.split("\n\n")
-nums = [int(n) for n in chunks[0].split(",")]
-boards = []
-for chunk in chunks[1:]:
-    boards.append([int(n) for n in " ".join(chunk.split()).split()])
-
-
-def check_board(board):
-    size = int(math.sqrt(len(board)))
-    for r in range(size):
-        start = r * size
-        if all(board[start : start + size]):
-            return True
-    for c in range(size):
-        if all([board[i] for i in range(c, len(board), size)]):
-            return True
-
-    return False
-
-
-hits = [[0] * len(board) for board in boards]
-winner = None
-wins = [False] * len(boards)
-last = None
-for num in nums:
-    for i, board in enumerate(boards):
-        if num in board:
-            idx = board.index(num)
-            hits[i][idx] = 1
-
-    for i, board in enumerate(hits):
-        if not wins[i] and check_board(board):
-            wins[i] = True
-            last = i
-
-    if num == 13:
-        print("13")
-
-    if all(wins):
-        break
-
-print(num, last)
-assert last
-hit = hits[last]
-board = boards[last]
-s = sum([board[i] for i, v in enumerate(hit) if not v])
-print(s, num)
-print(s * num)
-
-print(sorted([board[i] for i, v in enumerate(hit) if not v]))
-print(sorted(map(int, "3 15 22 18 19 8 25 20 12 6".split())))
-# print(sum(map(int, "3 15 22 18 19 8 25 20 12 6".split())))
+print(solve(lambda x: bool(x)))
+print(solve(lambda x: len(x) == len(boards)))

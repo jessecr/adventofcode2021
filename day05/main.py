ed099fa0420d97cc9e4e09c39d8db9f9fbd1b9e5
Jesse Capper-Ream <jessecapper@gmail.com>
2021-12-05 12:51:17 -0800

    Whitespace

--- day05/main.py
+++ day05/main.py
@@ -8,7 +8,6 @@ points = []
 for line in open(input_file).readlines():
     points.append([int(p) for ax in line.split(' -> ') for p in ax.split(',')])
 
-
 def coordinates(x1, y1, x2, y2, diagonals):
     if not diagonals and x1 != x2 and y1 != y2:
         return []
